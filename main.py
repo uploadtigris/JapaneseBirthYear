b46c5bfab74d21d639170c1abecd5cf47f7dc78e
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2022-04-15 18:51:31 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#################################################################################3
+# Title: Gregorian Birthdate to Japanese Calendar
+# Author: Tigris Mendez
+# Date: 4/15/2022
+# Purpose:
+#
+#      With this program, the user will be able to enter their birthdate in the
+#      Gregorian format mm-dd-yyyy and then receive the corresponding date in
+#      the Japanese calendar in relation to the different periods.
+#################################################################################
